bb65d558f73659317f525f27cf8020a6bebf48cd
SALMA ANIKA <ssalmaanika@gmail.com>
2025-10-31 18:59:43 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.matrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 
 st.title("Linear Regression Web Application")
 st.subheader("Application Built by Anika")
