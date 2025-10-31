7c6321eb780c4eb069a234e8d3de64da564b4243
SALMA ANIKA <ssalmaanika@gmail.com>
2025-10-31 16:55:02 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+from sklear.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 st.title("Linear Regression Web Application")
+st.subheader("Application Built by Anika")
+
+# Sidebar
+st.sidebar.header("Upload CSV Data or Use Sample")
