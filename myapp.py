22fcc1430ab98afd1773621393295a8e238a7ecb
SALMA ANIKA <ssalmaanika@gmail.com>
2025-10-31 17:20:46 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
